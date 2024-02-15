33a10781b759113b46b6808eb21e287760d0c5ad
Maudit00 <davide9311@gmail.com>
2024-02-15 10:16:58 +0100

    trying to make some tests

--- src/test/java/it/epicode/blogging/BloggingApplicationTests.java
+++ src/test/java/it/epicode/blogging/BloggingApplicationTests.java
@@ -1,13 +1,28 @@
 package it.epicode.blogging;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import it.epicode.blogging.models.AuthorRequest;
+import it.epicode.blogging.models.Authors;
+import it.epicode.blogging.repository.AuthorsRepository;
 
 @SpringBootTest
 class BloggingApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
+
+  @Autowired
+  AuthorsRepository authorsRepository;
+
+  @Test
+  public Authors saveAuthor(AuthorRequest authorRequest) {
+    Authors autore = new Authors(authorRequest.getNome(), authorRequest.getCognome(), authorRequest.getEmail(),
+        authorRequest.getDatadiNascita());
+    return authorsRepository.save(autore);
+  }
 
 }
